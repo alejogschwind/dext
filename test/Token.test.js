9b32d9ac613a660a3528812d52c5ba5d48b8d91e
Alejo Gschwind <31289074+sshuyn@users.noreply.github.com>
2021-09-19 20:42:32 -0300

    Feat/adding basic methods eip20 (#4)
    * Adding new token's name.
    
    * Adding token's symbol.
    
    * Adding token's decimals.
    
    * Adding token totalSupply.
    
    * Refactoring tests.

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,13 +5,37 @@ require('chai')
   .should();
 
 contract('Token', (accounts) => {
+  let token;
+  // Basic Token Info
+  const name = "Mango";
+  const symbol = "MNG";
+  const decimals = "18";
+  const totalSupply = "100000000" + "0".repeat(+decimals);
+  //
+  beforeEach(async () => {
+    token = await Token.new();
+  });
 
   describe('deployment', () => {
 
-    it('should track the contract\'s name', async () => {
-      const token = await Token.new();
+    it('should track token\'s name', async () => {
       const result = await token.name();
-      result.should.be.equal('DEXT');
+      result.should.be.equal(name);
+    });
+
+    it('should track token\'s symbol', async () => {
+      const result = await token.symbol();
+      result.should.be.equal(symbol);
+    });
+
+    it('should track token\'s decimals', async () => {
+      const result = await token.decimals();
+      result.toString().should.be.equal(decimals);
+    });
+
+    it('should track token totalSupply', async () => {
+      const result = await token.totalSupply();
+      result.toString().should.be.equal(totalSupply);
     });
   });
 });
