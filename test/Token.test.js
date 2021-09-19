6ab102553b1a192ffeb3e748b4e5dc8e155f981a
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-09-19 20:41:37 -0300

    Refactoring tests.

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,31 +5,37 @@ require('chai')
   .should();
 
 contract('Token', (accounts) => {
+  let token;
+  // Basic Token Info
+  const name = "Mango";
+  const symbol = "MNG";
+  const decimals = "18";
+  const totalSupply = "100000000" + "0".repeat(+decimals);
+  //
+  beforeEach(async () => {
+    token = await Token.new();
+  });
 
   describe('deployment', () => {
-    let token;
-    before(async () => {
-      token = await Token.new();
-    });
 
     it('should track token\'s name', async () => {
       const result = await token.name();
-      result.should.be.equal('Mango');
+      result.should.be.equal(name);
     });
 
     it('should track token\'s symbol', async () => {
       const result = await token.symbol();
-      result.should.be.equal('MNG');
+      result.should.be.equal(symbol);
     });
 
     it('should track token\'s decimals', async () => {
       const result = await token.decimals();
-      result.toString().should.be.equal('18');
+      result.toString().should.be.equal(decimals);
     });
 
     it('should track token totalSupply', async () => {
       const result = await token.totalSupply();
-      result.toString().should.be.equal('100000000000000000000000000');
+      result.toString().should.be.equal(totalSupply);
     });
   });
 });
