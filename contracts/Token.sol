262f4cf508ae7259aaab6cbb4894701fd12b571b
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-09-19 21:57:19 -0300

    Adding EIP20 transfer.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+
 contract Token {
+  using SafeMath for uint;
+  
   string public name = "Mango";
   string public symbol = "MNG";
   uint256 public decimals = 18;
@@ -11,5 +15,11 @@ contract Token {
   constructor() {
     totalSupply = 100_000_000 * (10 ** decimals);
     balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    balanceOf[_to] = _value;
+    return true;
   }
 }
